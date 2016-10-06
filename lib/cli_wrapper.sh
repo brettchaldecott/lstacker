cf1daf6c2ca69037f7301d0e7cd04a4188465e19
Brett Chaldecott <brett.chaldecott@gmail.com>
2016-10-06 06:30:09 +0000

    1) Changed cli executor to fix command line

--- lib/cli_wrapper.sh
+++ lib/cli_wrapper.sh
@@ -69,9 +69,8 @@ function cli_execute_command_with_input {
 	local input=$3
 
 	if [ ${server} == "local" ] ; then
-		local command_str="echo \"${input}\" | ${remote_command}"
-		echo_log "${command_str}"
-		local cli_result=`eval ${command_str}`
+		echo_log "echo ${input} | eval ${remote_command}"
+		local cli_result=`echo "${input}" | eval ${remote_command}`
 		local command_result=$?
 		echo "${cli_result}"
 		if [ "${IGNORE_RESULTS}" -ne "0" ] && [ ${command_result} -ne 0 ] ; then
